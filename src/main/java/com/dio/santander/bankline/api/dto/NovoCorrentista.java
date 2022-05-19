7abf2ff97a040dc2ff3cece2270c7b8205b2b04d
HericlisBlip <59284055+HericlisBlip@users.noreply.github.com>
2022-05-18 21:50:46 -0300

    Versao final do projeto.

---
+++ src/main/java/com/dio/santander/bankline/api/dto/NovoCorrentista.java
@@ -0,0 +1,23 @@
+package com.dio.santander.bankline.api.dto;
+
+public class NovoCorrentista {
+	public String nomeString;
+	public String cpf;
+
+	public String getNomeString() {
+		return nomeString;
+	}
+
+	public void setNomeString(String nomeString) {
+		this.nomeString = nomeString;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
